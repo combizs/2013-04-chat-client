63e634e6f507236eb22f5d8a9b60295979aef47e
Hack Reactor Students <github@hackreactor.com>
2013-05-10 15:29:27 -0700

    added friends feature with bold upon click

--- js/setup.js
+++ js/setup.js
@@ -14,27 +14,40 @@ $(document).ready(function() {
     jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
     jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
   });
-
+  var friends = {};
+  var roomName = $('#room').val();
   var ajaxQuery = function(){
     $.ajax('https://api.parse.com/1/classes/messages', {
       contentType: 'application/json',
-      data: {order: "-createdAt"},
+      data: {order: "-createdAt", where: {'roomname': roomName}},
       success: function(data){
         var userWithNames = _.filter(data.results, function(item){
           return item.hasOwnProperty('username') && item.hasOwnProperty('text') && item.hasOwnProperty('objectId') && item.hasOwnProperty('createdAt') && item.hasOwnProperty('updatedAt');
         });
-        console.log(userWithNames);
         $('#main ul').html("");
         _.each(userWithNames, function(item){
           var $message = $('<li></li>');
+          $message.addClass("chat");
           $message.text(item.username + ": " + item.text);
           $message.addClass(item.username);
+          $message.on("click", function(){
+              var newFriend = $(this).attr('class').split(' ')[1];
+              if (!friends[newFriend]) {
+                friends[newFriend] = true;
+              }
+              else {
+                friends[newFriend] = false;
+              }
+              console.log(friends);
+          });
+          if(friends[item.username]){
+            $message.addClass("bold");
+          }
           $('#main ul').append($message);
         });
 
         // console.log(data);
       },
-      timeout: 1000,
       error: function(data) {
         console.log('Ajax request failed');
       }
@@ -43,46 +56,44 @@ $(document).ready(function() {
   };
 
   ajaxQuery();
-var looper = function(){
-  $.ajax('https://api.parse.com/1/classes/messages', {
-    type: 'POST',
-    contentType: 'application/json',
-    data: JSON.stringify({username:"forever", text: "forever"}),
-    success: function(data) {
-      console.log('success');
-    },
-    error: function(data) {
-      console.log('Ajax request failed');
-    }
-  });
-};
 
-//setInterval(function(){looper();}, 100);
+  var submit = function(){
+    var message = {
+      'username': userName,
+      'text': $('#userText').val(),
+      'roomname': roomName
+    };
+    $('#userText').val("");
+    $.ajax('https://api.parse.com/1/classes/messages', {
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(message),
+      success: function(data) {
+        console.log('success');
+      },
+      error: function(data) {
+        console.log('Ajax request failed');
+      }
+    });
+  };
 
-
-  $('#userText').on("keyup",function(e) {
+  $('#userText').on("keyup", function(e) {
     if(e.keyCode !== 13){
       return;
     } else {
-      var message = {
-        'username': userName,
-        'text': $('#userText').val()
-      };
-      $('#userText').val("");
-      $.ajax('https://api.parse.com/1/classes/messages', {
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify(message),
-        success: function(data) {
-          console.log('success');
-        },
-        error: function(data) {
-          console.log('Ajax request failed');
-        }
-      });
+      submit();
     }
   });
+  $('.submit').on("click", function(e) {
+    submit();
+  });
+  $('#r')
+
+
+
 });
+
+
 /*
 cURL
 REST http methods: use GET, PUT, DELETE.
